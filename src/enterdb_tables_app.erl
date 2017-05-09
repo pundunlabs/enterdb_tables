9363b1422178d13ea9bc50fd55ee950c7440e159
jonasf <jonas.falkevik@mobilearts.com>
2017-05-09 13:25:29 +0200

    Enterdb tables open user tables
    open all user tables after initialization of internal dependencies.

---
+++ src/enterdb_tables_app.erl
@@ -0,0 +1,56 @@
+%%%===================================================================
+%% @author Jonas Falkevik
+%% @copyright 2017 Pundun Labs AB
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+%% implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%% -------------------------------------------------------------------
+%% @title
+%% @doc
+%% Module Description:
+%% @end
+%%%===================================================================
+
+-module(enterdb_tables_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+-include_lib("gb_log/include/gb_log.hrl").
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+start(_StartType, _StartArgs) ->
+    ?debug("About to open all tables."),
+    open_all_tables().
+
+stop(_State) ->
+    ok.
+%%--------------------------------------------------------------------
+%% @doc
+%% Open existing database table shards.
+%% @end
+%%--------------------------------------------------------------------
+-spec open_all_tables() -> ok | {error, Reason :: term()}.
+open_all_tables() ->
+    case enterdb_db:transaction(fun() -> mnesia:all_keys(enterdb_stab) end) of
+	{atomic, DBList} ->
+	    enterdb_lib:open_shards(DBList),
+	    enterdb_tables_sup:start_link();
+	{error, Reason} ->
+	    {error, Reason}
+    end.
+
+
